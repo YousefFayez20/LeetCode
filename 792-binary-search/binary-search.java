aaad040e6bf007f14d126896d8d08daaccc3caca
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-08-20 13:41:59 +0300

    Time: 0 ms (100.00%) | Memory: 45.8 MB (61.01%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,21 @@
+
+   class Solution {
+    public int search(int[] nums, int target) {
+        int L = 0;
+        int R = nums.length-1;
+        while(L <= R){
+            int mid = (R+L)/2;
+            if (nums[mid] > target){
+                R = mid - 1;
+            }
+            else if(nums[mid] < target){
+                L = mid + 1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+
+    }
+}
