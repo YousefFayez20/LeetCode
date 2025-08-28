a5595c30e6f4cb784b016bff1126225a9cdefa09
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-08-28 15:06:29 +0300

    Time: 4 ms (57.89%) | Memory: 44.7 MB (95.69%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,5 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //hasmap solution
+        int[] arr =new int[2];
+        HashMap<Integer,Integer> hash = new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            hash.put(nums[i],i);
+        }
+        for(int i =0;i<nums.length;i++){
+            int result = target - nums[i];
+            if(hash.containsKey(result) && hash.get(result) != i){
+                arr[0] = hash.get(result);
+                arr[1] = i;
+                break;
+            }
+        }
+        return  arr;
+        /*
+        //O(n^2) solution
         int[] index = new int[2];
         for(int i = 0; i< nums.length;i++){
             for(int j = i+1;j<nums.length;j++){
@@ -12,5 +29,6 @@ class Solution {
 
         }
         return index;
+        */
     }
 }
