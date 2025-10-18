594e29b09338eacb6228f428047dc3ed5858f0ef
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-10-18 21:02:52 +0300

    Time: 30 ms (60.74%) | Memory: 48.1 MB (94.92%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,25 +1,18 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        //we will make priority queue with a comparator based on  Euclidean distance
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a,b) -> {
-                return Double.compare(getDistance(a), getDistance(b));
-            });
-            for(int i = 0;i<points.length;i++){
-               pq.add(points[i]);
-            }
-            
-            int[][] Karr = new int[k][2];
-            for(int i = 0;i<k;i++){
-                Karr[i] = pq.poll();
-            }
-
-
-        
-        return Karr;
-        
+        Queue<int[]> pq = new PriorityQueue<>((a,b) -> {return Double.compare(calculateDistance(a) , calculateDistance(b));});
+        for(int[] point : points){
+                pq.add(point);
+        }
+        int[][] list = new int[k][2];
+        for(int i=0;i<k;i++){
+            int[] point = pq.poll();
+            list[i][0] = point[0];
+            list[i][1] = point[1];
+        }
+        return list;
     }
-    private double getDistance(int[] point) {
-        return Math.sqrt(point[0] * point[0] + point[1] * point[1]);
+    public double calculateDistance (int[] x){
+        return Math.sqrt(x[0]*x[0] + x[1]*x[1]);
     }
 }
