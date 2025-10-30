aa0bec78c8eeb13a53de342374a9e65f17e7308e
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-10-30 13:32:45 +0300

    Time: 64 ms (5.60%) | Memory: 55.3 MB (51.88%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,25 @@
+import java.util.Hashtable;
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Hashtable<Integer,Integer> baskets = new Hashtable<>();
+        // size == 2, calculate window size , <Fruit,count>
+        int windowStart = 0;
+        int windowSize = 0;
+        for(int i = 0;i<fruits.length;i++){
+            baskets.put(fruits[i], baskets.getOrDefault(fruits[i],0)+1);
+            if(baskets.size() <= 2){
+                windowSize = Math.max(windowSize, i-windowStart+1);
+            }
+            while(baskets.size() >2){
+                baskets.put(fruits[windowStart],baskets.getOrDefault(fruits[windowStart],0)-1);
+                if(baskets.get(fruits[windowStart]) == 0){
+                    baskets.remove(fruits[windowStart]);
+                }
+                windowStart++;
+                
+            }
+
+        }
+        return windowSize;
+    }
+}
