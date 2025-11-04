ccbbde3464ad032746f5c590adb26aa03013758f
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-11-04 13:00:14 +0200

    Time: 1 ms (18.53%) | Memory: 42.7 MB (5.08%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -5,15 +5,13 @@ class Solution {
         for(int L=0;L<haystack.length();L++){
             if(Rneedle < needle.length() && haystack.charAt(L) == needle.charAt(Rneedle)){
                 Rneedle++;
-                System.out.println("R = " + Rneedle +" L = " +L);
+                
                  if(Rneedle == needle.length()){
                 index = L - Rneedle +1;
                 break;
             }
             }else{
                 if(Rneedle>0){
-                 System.out.println("R = " + Rneedle +" L = " +L);
-
                     L = L - Rneedle ;
                     Rneedle =0;
                 }
