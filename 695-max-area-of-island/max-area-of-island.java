62ddb663db6714e704a95851076cb71d4795bf12
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-11-27 11:21:40 +0200

    Time: 2 ms (70.41%) | Memory: 46.4 MB (34.82%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,27 +1,29 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int maxArea = 0;
-        int rows = grid.length;
-        int columns = grid[0].length;
-        for(int i = 0;i<rows;i++){
-            for(int j = 0;j<columns;j++){
-                if(grid[i][j] == 1){
-                    maxArea = Math.max(dfs(i,j,grid),maxArea);
+        int ROWS = grid.length;
+        int COLS = grid[0].length;
+        boolean[][] visited = new boolean[ROWS][COLS];
+        for(int i=0;i<ROWS;i++){
+            for(int j=0;j<COLS;j++){
+                if(grid[i][j]==1 && !visited[i][j]){
+                    maxArea = Math.max(maxArea,dfs(grid,visited,i,j));
                 }
             }
         }
         return maxArea;
     }
-    public int dfs(int r, int c, int[][] grid){
-        if(Math.min(r,c) < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == 0){
+    int dfs(int[][] grid,boolean[][] visited,int r,int c){
+        if(Math.min(r,c) < 0 || r==grid.length || c== grid[0].length||visited[r][c]||grid[r][c] == 0){
             return 0;
         }
-        grid[r][c] = 0;
         int area = 1;
-        area += dfs(r+1,c,grid);
-        area += dfs(r-1,c,grid);
-        area += dfs(r,c+1,grid);
-        area += dfs(r,c-1,grid);
-        return area;
+        visited[r][c] = true;
+        area += dfs(grid,visited,r+1,c);
+        area += dfs(grid,visited,r-1,c);
+        area += dfs(grid,visited,r,c+1);
+        area += dfs(grid,visited,r,c-1);
+        return area; 
+
     }
 }
