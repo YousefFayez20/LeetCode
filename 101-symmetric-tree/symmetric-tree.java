4afbee277ed9000fc7162242c308660bfcdf72e6
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-11-29 11:25:22 +0200

    Time: 0 ms (100.00%) | Memory: 43.5 MB (27.65%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,23 +15,13 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null || (root != null &&(root.left ==null && root.right == null))){
-            return true;
-        }
-        if(root.left !=null && root.right != null){
-            return dfs(root.left,root.right);
-        }else{
-            return false;
-        }
-        
+        if(root == null) return true;
+        return dfs(root.left,root.right);
     }
-    public boolean dfs(TreeNode left,TreeNode right) {
-        if(left == null && right == null){
-            return true;
-        }
-        if(left == null || right == null){
-            return false;
-        }
-        return(left.val == right.val && dfs(left.left, right.right)&& dfs(left.right,right.left));
+    boolean dfs(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        if(left.val != right.val) return false;
+    return dfs(left.left,right.right) && dfs(left.right,right.left);
     }
 }
