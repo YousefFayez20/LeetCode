198ded354e8998b40bbc2e80db6020fa64230130
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-12-01 09:31:54 +0200

    Time: 23 ms (23.80%) | Memory: 47.7 MB (17.11%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,38 +1,36 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        Queue<int[]> queue = new LinkedList<>();
+        if(grid[0][0] ==1) return -1;
+        Queue<int[]> queue = new ArrayDeque<>();
         int ROWS = grid.length;
         int COLS = grid[0].length;
-        int[][] visited = new int[ROWS][COLS];
-        queue.offer(new int[2]); //(0,0) start point
-        int length = 1;
-        visited[0][0] = 1;
-        if(grid[0][0] == 1 || grid[ROWS-1][COLS-1] == 1){
-            return -1;
-        }
+        boolean[][] visited = new boolean[ROWS][COLS];
+        queue.add(new int[2]);
+        visited[0][0] = true;
+        int length=1;
         while(!queue.isEmpty()){
-            int QueueLength = queue.size();
-            for(int i = 0;i<QueueLength;i++){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
                 int[] pair = queue.poll();
-                int R = pair[0];
-                int C = pair[1];
-                if(R == ROWS-1 && C == COLS -1){
+                int r = pair[0],  c = pair[1];
+                if(r == ROWS-1 && c == COLS-1 && grid[r][c] ==0){
                     return length;
-                }
-                int [][] neighbors = {{R+1,C},{R+1,C+1},{R+1,C-1},{R,C+1},{R-1,C},{R-1,C-1}, {R,C-1},{R-1,C+1}};
-                for(int j = 0; j<neighbors.length;j++){
-                    int newR = neighbors[j][0];
-                    int newC = neighbors[j][1];
-                    if(newR < 0 ||newC < 0|| newR == ROWS || newC == COLS ||visited[newR][newC] == 1 ||grid[newR][newC] == 1){
+                } 
+                int[][] neighbors = {{r,c+1},{r,c-1},{r+1,c},{r-1,c},{r-1,c-1},
+                {r+1,c+1},{r+1,c-1},{r-1,c+1}};
+                for(int j = 0;j<neighbors.length;j++){
+                    int newR = neighbors[j][0], newC = neighbors[j][1];
+                    if(Math.min(newR,newC) < 0 || newR >= ROWS||newC >= COLS 
+                    ||visited[newR][newC] == true ||grid[newR][newC] == 1){
                         continue;
                     }
-                    queue.offer(neighbors[j]);
-                    visited[newR][newC] = 1;
+                    queue.add(neighbors[j]);
+                    visited[newR][newC] =true;
                 }
-
             }
             length++;
         }
         return -1;
+
     }
 }
