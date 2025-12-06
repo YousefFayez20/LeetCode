983b6b10e17bc82ff7d67c4624c98a4e3b72b011
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-12-06 15:53:55 +0200

    Time: 215 ms (53.88%) | Memory: 42.8 MB (22.10%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        
+        solve(board);
+    }
+    boolean solve(char[][] board){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            if(board[i][j] == '.'){
+
+                for(char num = '1'; num <= '9'; num++){
+                    if(isValid(board, i, j, num)){
+                        
+                        board[i][j] = num;
+
+                        if(solve(board)){
+                            return true;
+                        }
+
+                        board[i][j] = '.'; // backtrack
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+    boolean isValid(char[][] board,int row,int column,char value){
+        for(int i =0;i<board.length;i++){
+            if(board[row][i] == value){
+                return false;}
+                 if(board[i][column] == value){
+                return false;
+            }
+            }
+        
+         
+        int r= (row/3)*3;
+        int c= (column/3)*3;
+
+        for(int i =r;i<r+3;i++){
+            for(int j =c;j<c+3;j++){
+                if(board[i][j] == value) return false;
+            }
+        }
+        return true;
+        
+    }
+}
