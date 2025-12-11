f27e9cbc6b32863c0c3b80a3e8e308738e475d08
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-12-11 13:20:38 +0200

    Time: 143 ms (53.14%) | Memory: 43.1 MB (33.45%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,33 +1,28 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+
+        boolean[][] visited = new boolean[board.length][board[0].length];
         boolean found = false;
-        int rows = board.length;
-        int columns = board[0].length;
-        boolean [][] visited = new boolean[rows][columns];
-        char firstChar = word.charAt(0);
-        for(int i = 0;i<rows;i++){
-            for(int j = 0;j<columns;j++){
-                if(board[i][j] == firstChar){
-                    found = dfs(board,word,i,j,0, visited);
-                    if (found == true){
-                        return true;
-                    }
-                }
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                found = backtracking(board,word,i,j,0,visited);
+                if(found == true) return true;
             }
         }
-        return found;
+        return false;
+        
     }
-     public boolean dfs(char[][] board, String word, int r, int c ,int index,boolean[][] visited ){
-        if(index == word.length()){
-            return true;
-        }
-        if(Math.min(r,c) < 0||r >= board.length|| c>= board[0].length || word.charAt(index) != board[r][c] || visited[r][c]){
-            return false;
-        }
+    boolean backtracking(char[][] board, String word,int r,int c, int idx,boolean[][] visited){
+        if(Math.min(r,c)<0||r==board.length||c==board[0].length|| word.charAt(idx) != board[r][c] || visited[r][c]) return false;
+        if(idx == word.length()-1) return true;
         visited[r][c] = true;
-        boolean result = (dfs(board,word,r+1,c,index+1, visited) || dfs(board,word,r-1,c,index+1, visited) || dfs(board,word,r,c+1,index+1, visited) || dfs(board,word,r,c-1,index+1, visited));
-        visited[r][c] = false; 
-        return result;
+        boolean moveLeft = backtracking(board,word,r,c-1,idx+1,visited);
+        boolean moveRight = backtracking(board,word,r,c+1,idx+1,visited);
+        boolean moveUp = backtracking(board,word,r-1,c,idx+1,visited);
+        boolean moveDown = backtracking(board,word,r+1,c,idx+1,visited);
+        visited[r][c] = false;
+        return moveLeft ||moveRight||moveUp||moveDown;
 
-     }
+
+    }
 }
