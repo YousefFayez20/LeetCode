02b5d5d6810e48572cee30dfe8537b9e0ac9c43c
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2026-07-14 18:05:07 +0300

    Time: 1 ms (26.41%) | Memory: 43.9 MB (28.98%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,25 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums3 = new int[m];
-        for(int i=0;i<m;i++){
-            nums3[i]=nums1[i];
-        }
-        int n1=0;
-        int n2=0;
-        for(int i=0;i<nums1.length;i++){
-            if(n1<m&&n2<n&& nums3[n1]<nums2[n2]){
-                nums1[i] = nums3[n1];
-                n1++;
-            }else if(n1<m && n2<n && nums3[n1]>=nums2[n2]){
-                nums1[i] = nums2[n2];
-                n2++;
-            } else if(n1<m){
-                nums1[i] = nums3[n1];
-                n1++;
-            }else{
-                nums1[i] = nums2[n2];
-                n2++;
+    int L1 =0;
+    int L2 =0;
+    int size = m;
+    while(L1<nums1.length && L2 <n){
+        if(nums1[L1]<=nums2[L2]){
+            L1++;
+        }else{
+            for(int i = nums1.length-2;i>=L1;i--){
+                nums1[i+1] = nums1[i];
             }
+            nums1[L1]=nums2[L2];
+            L1++;
+            L2++;
+            size++;
         }
+    }
+    while(L2 < n){
+        nums1[size] = nums2[L2];
+        size++;
+        L2++;
+    }
     }
 }
